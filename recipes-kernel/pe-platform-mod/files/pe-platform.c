62d84560896e82c48cfa81bd70060f5cf49f5baf
Saggggo <sagoryota@gmail.com>
2020-02-11 15:05:58 +0900

    config w/r

--- recipes-kernel/pe-platform-mod/files/pe-platform.c
+++ recipes-kernel/pe-platform-mod/files/pe-platform.c
@@ -5,13 +5,13 @@
 #include <linux/slab.h>
 #include <linux/sysfs.h>
 
-#define NUMBEROF_SYSFS_ENTRY 64
+#define NUMBEROF_CONFIG_ENTRY 64
 
 static struct kobject *hoshilab_kobj;
 static void __iomem *base_addr;
 
-static struct attribute *config_attrs[NUMBEROF_SYSFS_ENTRY + 1];
-static struct attribute *data_attrs[NUMBEROF_SYSFS_ENTRY + 1];
+static struct attribute *config_attrs[NUMBEROF_CONFIG_ENTRY + 1];
+static struct attribute *data_attrs[NUMBEROF_CONFIG_ENTRY + 1];
 
 static struct attribute_group config = {
 	.name = "config",
@@ -38,6 +38,24 @@ static ssize_t config_read(struct kobject *kobj, struct kobj_attribute *kattr, c
 	return sprintf(buf, "%p: %x\n", container->address, *(u32 *)container->address);
 }
 
+static ssize_t config_write(struct kobject *kobj, struct kobj_attribute *kattr, const char *buf, size_t size) {
+	u32 value;
+	int num;
+	struct container_sysfs *container;
+	container = container_of(kattr, struct container_sysfs, kattr);
+	num = sscanf(buf, "%x", &value);
+	if (!num)
+		return -1;
+	*(u32 *)(container->address) = value;
+	return size;
+}
+
+static ssize_t data_read(struct kobject *kobj, struct kobj_attribute *kattr, char *buf) {
+	struct container_sysfs *container;
+	container = container_of(kattr, struct container_sysfs, kattr);
+	return sprintf(buf, "%p: %x\n", container->address, *(u32 *)container->address);
+}
+
 struct hoshilab_pe_platform_data {
 	const char *name;
 } pe_platform_data = {"pe-platform"};
@@ -58,18 +76,18 @@ static int pe_platform_probe(struct platform_device *pdev) {
 	if (IS_ERR(base_addr))
 			return PTR_ERR(base_addr);
 
-	config_sysfs = (struct container_sysfs *)kzalloc(sizeof(struct container_sysfs) * NUMBEROF_SYSFS_ENTRY, GFP_KERNEL);
-	config_name = (char(*)[3])kzalloc(sizeof(char)*3*NUMBEROF_SYSFS_ENTRY, GFP_KERNEL);
-	for (i = 0; i < NUMBEROF_SYSFS_ENTRY; i++) {
+	config_sysfs = (struct container_sysfs *)kzalloc(sizeof(struct container_sysfs) * NUMBEROF_CONFIG_ENTRY, GFP_KERNEL);
+	config_name = (char(*)[3])kzalloc(sizeof(char)*3*NUMBEROF_CONFIG_ENTRY, GFP_KERNEL);
+	for (i = 0; i < NUMBEROF_CONFIG_ENTRY; i++) {
 		config_name[i][0] = (char)(i/10 + '0');
 		config_name[i][1] = (char)(i%10 + '0');
 		config_name[i][2] = '\0';
 
-		config_sysfs[i].address = base_addr + i;
+		config_sysfs[i].address = base_addr + i*4;
 		config_sysfs[i].kattr.attr.name = config_name[i];
-		config_sysfs[i].kattr.attr.mode = 0444;
+		config_sysfs[i].kattr.attr.mode = 0644;
 		config_sysfs[i].kattr.show =  config_read;
-		config_sysfs[i].kattr.store = NULL;
+		config_sysfs[i].kattr.store = config_write;
 		config_attrs[i] = &config_sysfs[i].kattr.attr;
 	}
 	config_attrs[i] = NULL;
